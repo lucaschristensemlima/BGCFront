6e91f886b3f53e99e272e2d1b3d431885a8a51c3
Lucas <lucas.christensem@poli.ufrj.br>
2020-08-25 21:51:43 -0300

    products

--- src/config.js
+++ src/config.js
@@ -5,12 +5,12 @@ export default {
     },
     apiGateway: {
       REGION: "us-east-1",
-      URL: " https://8o1ja9a0l3.execute-api.us-east-1.amazonaws.com/prod/"
+      URL: " https://8o1ja9a0l3.execute-api.us-east-1.amazonaws.com/prod"
     },
     cognito: {
       REGION: "us-east-1",
       USER_POOL_ID: "us-east-1_vZ2C30nx4",
       APP_CLIENT_ID: "4ufpklj4bu3138bpr48tve69i8",
-      IDENTITY_POOL_ID: "us-east-2:4d38fd2e-e8cd-4b6c-9d32-209a0d123e49"
+      IDENTITY_POOL_ID: "us-east-1:17745651-604d-488b-b35d-2c16dec563b7"
     }
   };
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
+import { useAppContext } from "../libs/contextLib";
+import { onError } from "../libs/errorLib";
 import "./Home.css";
+import { API } from "aws-amplify";
+import { LinkContainer } from "react-router-bootstrap";
+
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+  const { isAuthenticated } = useAppContext();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function onLoad() {
+      if (!isAuthenticated) {
+        return;
+      }
+  
+      try {
+        const products = await loadProducts();
+        setProducts(products);
+      } catch (e) {
+        onError(e);
+      }
+  
+      setIsLoading(false);
+    }
+  
+    onLoad();
+  }, [isAuthenticated]);
+  
+  function loadProducts() {
+    return API.get("products", "/products");
+  }
+
+  function renderProductsList(products) {
+    return products.map( (product, i) => (
+      <LinkContainer key={product.id} to={`/products/${product.id}`}>
+              <ListGroupItem header={product.name}>
+                  {"Price: R$ " + product.price + " - Qtd: " + product.qtd}
+              </ListGroupItem>
+            </LinkContainer>
+    ));
+    }    
+  function renderLander() {
+    return (
+      <div className="lander">
+        <h1>Minion Store</h1>
+      </div>
+    );
+  }
+
+  function renderProducts() {
+    return (
+      <div className="products">
+        <PageHeader>Products</PageHeader>
+        <ListGroup>
+          {!isLoading && renderProductsList(products)}
+        </ListGroup>
+      </div>
+    );
+  }
+
   return (
     <div className="Home">
-      <div className="lander">
-        <h1>Minions Store</h1>
-        <p>buy minions here, banana!</p>
-      </div>
+      {isAuthenticated ? renderProducts() : renderLander()}
     </div>
   );
 }
